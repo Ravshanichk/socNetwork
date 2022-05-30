2ef76eaa8023a36f4dd9865e80f3459091607d83
aramaz <rbkkth9@gmail.com>
2022-05-30 16:29:34 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './Navbar.module.css'
+
+const Navbar = () =>{
+    return(
+        <div className={s.navbar}>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
