378d12d994fef21a1aa704427e3eaddb7dcc3626
aramaz <rbkkth9@gmail.com>
2022-06-28 23:06:09 +0600

    29. created state.js for BLL data

--- src/App.js
+++ src/App.js
@@ -10,28 +10,9 @@ import Profile from "./components/Profile/Profile";
 import Settings from "./components/Settings/Settings";
 import Users from "./components/Users/Users";
 
-const App = () => {
+const App = (props) => {
 
-  let posts = [
-    {id:1, message: 'Sometime owls are not what they look like', likeCount:3},
-    {id:2, message: 'YOU!', likeCount:0},
-    {id:3, message: 'Just let it go', likeCount:1},
-  ]
 
-  let dialogItems = [
-    { name: "Stein", id: 1 },
-    { name: "Akira", id: 2 },
-    { name: "Husein", id: 3 },
-    { name: "Salt", id: 4 },
-  ];
-
-  let messages = [
-    { text: "Hello", id: 1 },
-    { text: "Hi!", id: 2 },
-    { text: "Sometimes i wish i were a bird", id: 3 },
-    { text: "My daughter is going to America", id: 4 },
-    { text: "You look like previous prime minister of Japan", id: 5 },
-  ];
 
 
   return (
@@ -40,8 +21,8 @@ const App = () => {
       <Navbar />
       <div className={s.content}>
         <Routes>
-          <Route path="/profile" element={<Profile posts={posts} />} />
-          <Route path="/dialogs/*" element={<Dialogs dialogItems={dialogItems} messages={messages}/>} />
+          <Route path="/profile" element={<Profile posts={props.appState.profilePage} />} />
+          <Route path="/dialogs/*" element={<Dialogs dialogItems={props.appState.dialogsPage.dialogsItems} messages={props.appState.dialogsPage.messages}/>} />
           <Route path="/news" element={<News />} />
           <Route path="/Music" element={<Music />} />
           <Route path="/settings" element={<Settings />} />
---
+++ src/redux/state.js
@@ -0,0 +1,32 @@
+let state = {
+  profilePage: [
+    {
+      id: 1,
+      message: "Sometime owls are not what they look like",
+      likeCount: 3,
+    },
+    { id: 2, message: "YOU!", likeCount: 0 },
+    { id: 3, message: "Just let it go", likeCount: 1 },
+  ],
+  dialogsPage: {
+    messages: [
+            { text: "Hello", id: 1 },
+            { text: "Hi!", id: 2 },
+            { text: "Sometimes i wish i were a bird", id: 3 },
+            { text: "My daughter is going to America", id: 4 },
+            { text: "You look like previous prime minister of Japan", id: 5 },
+            ],
+    
+    dialogsItems: [
+            { name: "Stein", id: 1 },
+            { name: "Akira", id: 2 },
+            { name: "Husein", id: 3 },
+            { name: "Salt", id: 4 },
+                ],
+    }
+  
+
+ 
+};
+
+export default state
